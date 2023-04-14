27134c97c3af8b57f31dc8211c9444b8a9c58145
Sergio Arango <estudiospersonales01@gmail.com>
2023-04-13 20:16:02 -0500

    first commit

---
+++ src/TodoCounter.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function TodoCounter(){
+    return(
+        <h2>2 of 3 ToDo completed</h2>
+    );
+}
+
+export { TodoCounter };
---
+++ src/TodoSearch.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function TodoSearch() {
+  return(
+    <input placeholder='Write what you want here'/>
+  );
+}
+
+export { TodoSearch };
