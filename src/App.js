93e69421fb3c69a39a4f5d1c2957b361cef43eeb
Sergio Arango <estudiospersonales01@gmail.com>
2023-04-20 23:08:31 -0500

    Implemented functionalities for search todos and modify the count of completed todos

--- src/App.js
+++ src/App.js
@@ -6,20 +6,42 @@ import { TodoItem } from "./TodoItem";
 import { CreateTodoButton } from "./CreateTodoButton";
 import './App.css';
 
-const todos = [
+const defaultTodos = [
   {id: 1, text: 'Estudiar JavaScript', completed: true},
-  {id: 2, text: 'Estudiar React', completed: true},
+  {id: 2, text: 'Estudiar React', completed: false},
   {id: 3, text: 'Estudiar NestJs', completed: false},
 ];
 
 function App() {
+  const [todos, setTodos] = React.useState(defaultTodos);
+  const [searchValue, setSearchValue] = React.useState('');
+  const completedTodos = todos.filter(todo => todo.completed).length;
+  const totalTodos = todos.length;
+
+  let searchedTodos = [];
+  if (searchValue.length === 0) {
+    searchedTodos = todos;
+  } else {
+    searchedTodos = todos.filter(todo => {
+    const todoText = todo.text.toLowerCase();
+    const searchText = searchValue.toLowerCase();
+    return todoText.includes(searchText);
+    });
+  }
+
   return (
     <React.Fragment>
       <div className="main-container">
-        <TodoCounter/>
-        <TodoSearch/>
+        <TodoCounter
+          total={totalTodos}
+          completed={completedTodos}
+        />
+        <TodoSearch
+          searchValue={searchValue}
+          setSearchValue={setSearchValue}
+        />
         <TodoList>
-          {todos.map(todo => (
+          {searchedTodos.map(todo => (
             <TodoItem 
               key={todo.id} 
               text={todo.text}
