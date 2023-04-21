d4b363e08b437b2f0cf6122261db67137274bb8b
Sergio Arango <estudiospersonales01@gmail.com>
2023-04-20 23:04:07 -0500

    Implemented the search functionality

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,9 +1,19 @@
 import React from "react";
 import './TodoSearch.css';
 
-function TodoSearch() {
-  return(
-    <input className="todo-search" placeholder='Search a ToDo'/>
+function TodoSearch({searchValue, setSearchValue}) {
+  
+  const onSearchValueChange = (event) => {
+    setSearchValue(event.target.value);
+  }
+
+  return (
+    <input 
+    className="todo-search" 
+    placeholder='Search a ToDo'
+    value={searchValue}
+    onChange={onSearchValueChange}
+    />
   );
 }
 
