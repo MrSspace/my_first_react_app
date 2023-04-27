744543e01a48670043c53a7b73347b27528f7236
Sergio Arango <estudiospersonales01@gmail.com>
2023-04-26 23:51:02 -0500

    Added data persistence using localstorage.

--- src/App/App.js
+++ src/App/App.js
@@ -1,14 +1,34 @@
 import React from "react";
 import { AppUI } from "./AppUI";
 
-const defaultTodos = [
-  {id: 1, text: 'Estudiar JavaScript', completed: true},
-  {id: 2, text: 'Estudiar React', completed: false},
-  {id: 3, text: 'Estudiar NestJs', completed: false},
-];
+// const defaultTodos = [
+//   {id: 1, text: 'Estudiar JavaScript', completed: true},
+//   {id: 2, text: 'Estudiar React', completed: false},
+//   {id: 3, text: 'Estudiar NestJs', completed: false},
+// ];
 
 function App() {
-  const [todos, setTodos] = React.useState(defaultTodos);
+
+  const TODOS_IN_STORAGE = {
+    V1 : 'TODOS_V1',
+  };
+  
+  const localStorageTodos = localStorage.getItem(TODOS_IN_STORAGE.V1);
+  let parsedTodos;
+  if(!localStorageTodos){
+    localStorage.setItem(TODOS_IN_STORAGE.V1,JSON.stringify([]));
+    parsedTodos = [];
+  } else {
+    parsedTodos = JSON.parse(localStorageTodos);
+  }
+
+  const saveTodos = (newTodos) => {
+    const strTodos = JSON.stringify(newTodos);
+    localStorage.setItem(TODOS_IN_STORAGE.V1, strTodos);
+    setTodos(newTodos);
+  };
+
+  const [todos, setTodos] = React.useState(parsedTodos);
   const [searchValue, setSearchValue] = React.useState('');
   const completedTodos = todos.filter(todo => todo.completed).length;
   const totalTodos = todos.length;
@@ -28,12 +48,12 @@ function App() {
     const index = todos.findIndex(todo => todo.text === text);
     const modifiedTodos = [...todos];
     modifiedTodos[index].completed = (modifiedTodos[index].completed ? false : true);
-    setTodos(modifiedTodos);
+    saveTodos(modifiedTodos);
   }
 
   const deleteTodo = (text) => {
     const modifiedTodos = todos.filter(todo=>todo.text !== text);
-    setTodos(modifiedTodos);
+    saveTodos(modifiedTodos);
   }
 
   return (
